6dddc403aaf2548e72358f2149ec02ac163d672b
Jeonyg <jyg11240518@gmail.com>
2025-12-09 11:28:29 +0900

    profilepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,9 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import BookDetail from "./components/BookDetail";
 import ProfilePage from "./pages/ProfilePage";
-import ProfileEdit from "./pages/ProfileEdit";
-// 나중에 AuthProvider 쓸 거면 이 줄 살려두고, 아니면 지워도 됨
-// import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    // <AuthProvider>
     <BrowserRouter>
       <Layout>
         <Routes>
@@ -22,12 +18,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/profile/edit" element={<ProfileEdit />} />
           <Route path="/:bookId" element={<BookDetail />} />
         </Routes>
       </Layout>
     </BrowserRouter>
-    // </AuthProvider>
   );
 }
 
